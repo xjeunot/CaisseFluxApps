7152a7b301c4037fa685abff3c9b5dfc2ae7e664
FRUP44168 <FRUP44168@GFI525018>
2018-11-09 16:17:45 +0100

    Ajout Support MongoDB

---
+++ Services/Magasin/Magasin.API/Bdd/Connexion/IMongoDbClient.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace Client.API.Bdd.Connexion
+{
+    public interface IMongoDbClient : IDisposable
+    {
+        bool EstConnecte { get; }
+
+        bool EssaiConnexion();
+
+        IMongoDatabase DonneDatabase();
+    }
+}
---
+++ Services/Magasin/Magasin.API/Bdd/Connexion/MongoDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Security;
+
+namespace Client.API.Bdd.Connexion
+{
+    public class MongoDbConfig
+    {
+        public string ChaineConnexion { get; set; }
+
+        public string BaseDeDonnees { get; set; }
+
+        public string Utilisateur { get; set; }
+
+        public string MotDePasse { get; set; }
+    }
+}
